5c06d387512a10b06d1938427f0442c80be6a541
Santiago <71831129+mata152dor55@users.noreply.github.com>
2024-06-20 19:46:50 -0500

    Add files via upload

---
+++ mod4_solution/easier/script.js
@@ -0,0 +1,32 @@
+
+var helloSpeaker = (function() {
+  var speakWord = "Hello";
+  return {
+      speak: function(name) {
+          console.log(speakWord + " " + name);
+      }
+  };
+})();
+
+var byeSpeaker = (function() {
+  var speakWord = "Good Bye";
+  return {
+      speak: function(name) {
+          console.log(speakWord + " " + name);
+      }
+  };
+})();
+
+
+(function iife1() {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+  for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+          byeSpeaker.speak(names[i]);
+      } else {
+          helloSpeaker.speak(names[i]);
+      }
+  }
+})();
